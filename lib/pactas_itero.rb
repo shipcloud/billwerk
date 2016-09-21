14f9c94f2416dbc63ec751198e9a3ae6bf6e4935
Simon Fröhler <sfroehler@gmail.com>
2016-09-21 11:19:20 +0200

    Removes support for ruby < 2.0
    Removes support for ruby < 2.0 and refactors method_missing
    and respond_to_missing? methods.

--- lib/pactas_itero.rb
+++ lib/pactas_itero.rb
@@ -14,16 +14,18 @@ module PactasItero
       @client
     end
 
-    # @private
-    def respond_to_missing?(method_name, include_private=false); client.respond_to?(method_name, include_private); end if RUBY_VERSION >= "1.9"
-    # @private
-    def respond_to?(method_name, include_private=false); client.respond_to?(method_name, include_private) || super; end if RUBY_VERSION < "1.9"
+    def respond_to_missing?(method_name, include_private = false)
+      (respond_to?(:client) && client.respond_to?(method_name, include_private)) || super
+    end
 
-  private
+    private
 
     def method_missing(method_name, *args, &block)
-      return super unless client.respond_to?(method_name)
-      client.send(method_name, *args, &block)
+      if respond_to?(:client) && client.respond_to?(method_name)
+        client.send(method_name, *args, &block)
+      else
+        super
+      end
     end
   end
 end
--- pactas_itero.gemspec
+++ pactas_itero.gemspec
@@ -21,6 +21,8 @@ Gem::Specification.new do |spec|
   spec.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   spec.require_paths = ['lib']
 
+  spec.required_ruby_version = ">= 2.0"
+
   spec.add_dependency('faraday_middleware', '~> 0.9.1')
   spec.add_dependency('rash', '~> 0.4.0')
 
