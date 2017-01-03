e0aff8c7d67f1a498587f8ab12aa45a5ecad8c56
Simon Fröhler <simon@shipcloud.io>
2017-01-03 10:20:33 +0100

    Fixes "Stack level too deep" issue (#13)
    * Fixes issue where stubbing the PactasItero.client method
    caused a "Stack level too deep" error on rspec > 3.3
    
    * Fixes failing specs due to gem updates

--- lib/pactas_itero.rb
+++ lib/pactas_itero.rb
@@ -2,7 +2,6 @@ require 'pactas_itero/client'
 require 'pactas_itero/default'
 
 module PactasItero
-
   class << self
     include PactasItero::Configurable
 
@@ -10,18 +9,17 @@ module PactasItero
     #
     # @return [PactasItero::Client] API wrapper
     def client
-      @client = PactasItero::Client.new(options)
-      @client
+      PactasItero::Client.new(options)
     end
 
     def respond_to_missing?(method_name, include_private = false)
-      (respond_to?(:client) && client.respond_to?(method_name, include_private)) || super
+      method_name != :client && client.respond_to?(method_name, include_private) || super
     end
 
     private
 
     def method_missing(method_name, *args, &block)
-      if respond_to?(:client) && client.respond_to?(method_name)
+      if method_name != :client && client.respond_to?(method_name)
         client.send(method_name, *args, &block)
       else
         super
--- spec/pactas_itero/api/oauth_spec.rb
+++ spec/pactas_itero/api/oauth_spec.rb
@@ -1,39 +1,43 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe PactasItero::Api::OAuth do
-
-  describe '#token' do
-    before do
-      stub_post("https://a_client_id:a_client_secret@sandbox.billwerk.com/oauth/token").with(
-        :body => { :grant_type => 'client_credentials' }
-      ).to_return(
-        :status => 200,
-        :body => fixture('bearer_token.json'),
-        :headers => {
-          :content_type => 'application/json; charset=utf-8'
-        }
+  describe "#token" do
+    it "requests the correct resource" do
+      client_credentials_request = stub_post("https://sandbox.billwerk.com/oauth/token").with(
+        basic_auth: %w(a_client_id a_client_secret),
+        body: "grant_type=client_credentials",
+        headers: {
+          "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
+        },
       )
-    end
 
-    it 'requests the correct resource' do
-      client = PactasItero::Client.new(client_id: 'a_client_id', client_secret: 'a_client_secret')
+      client = PactasItero::Client.new(client_id: "a_client_id", client_secret: "a_client_secret")
 
       client.token
 
-      expect(a_post("https://a_client_id:a_client_secret@sandbox.billwerk.com/oauth/token").with(
-        :body => "grant_type=client_credentials",
-        :headers => {
-          'Content-Type'=>'application/x-www-form-urlencoded; charset=UTF-8'
-        }
-      )).to have_been_made
+      expect(client_credentials_request).to have_been_made
     end
 
-    it 'returns the bearer token' do
-      client = PactasItero::Client.new(client_id: 'a_client_id', client_secret: 'a_client_secret')
+    it "returns the bearer token" do
+      stub_post("https://sandbox.billwerk.com/oauth/token").with(
+        basic_auth: %w(a_client_id a_client_secret),
+        body: "grant_type=client_credentials",
+        headers: {
+          "Content-Type" => "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+      ).to_return(
+        status: 200,
+        body: fixture("bearer_token.json"),
+        headers: {
+          content_type: "application/json; charset=utf-8",
+        },
+      )
+
+      client = PactasItero::Client.new(client_id: "a_client_id", client_secret: "a_client_secret")
       bearer_token = client.token
 
-      expect(bearer_token.access_token).to eq('top_secret_access_token')
-      expect(bearer_token.token_type).to eq('bearer')
+      expect(bearer_token.access_token).to eq("top_secret_access_token")
+      expect(bearer_token.token_type).to eq("bearer")
       expect(bearer_token.expires).to eq(0)
     end
   end
