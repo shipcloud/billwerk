b775e24eee37e75b28cb8076ee4f71791fcf2bec
soenkex <soenkex@freenet.de>
2018-01-23 00:21:55 +0100

    do not camelize exact string "_c"

--- lib/pactas_itero/api/coupons.rb
+++ lib/pactas_itero/api/coupons.rb
@@ -11,11 +11,6 @@ module PactasItero
         get "api/v1/coupons/#{coupon_id}", options
       end
 
-      def coupon(coupon_id, options = {})
-        options = options.camelize_keys
-        get "api/v1/coupons/#{coupon_id}", options
-      end
-
       def coupon_by_code(coupon_code, options = {})
         options = options.camelize_keys
         get "api/v1/coupons/?couponCode=#{coupon_code}", options
