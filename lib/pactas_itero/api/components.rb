f2652057d7afb3432877ce40088158831aba7032
soenkex <soenkex@freenet.de>
2018-03-04 16:46:10 +0100

    add create method for coupons, discounts and components

--- lib/pactas_itero/api/components.rb
+++ lib/pactas_itero/api/components.rb
@@ -10,6 +10,11 @@ module PactasItero
         options = options.camelize_keys
         get "api/v1/components/#{component_id}", options
       end
+      
+      def create_component(plangroup_id, options = {})
+        options = options.camelize_keys
+        post "api/v1/planGroups/#{plangroup_id}/components", options
+      end
 
       def update_component(component_id, options = {})
         options = options.camelize_keys
